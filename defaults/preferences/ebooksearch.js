3bc69fd14911a2c89fb34ea308f8a5d3f4d510e8
priyadarsan.v <priyadarsan.v@ca200bd9-eb3a-0410-80b8-db5cc7005a8e>
2008-07-15 11:44:56 +0000

    Added GSearch; Functionality changes in Search Engine

--- defaults/preferences/ebooksearch.js
+++ defaults/preferences/ebooksearch.js
@@ -1,4 +1,4 @@
-pref("ebooksearch.engine.installed",false);
 pref("ebooksearch.newtab",true);
+pref("ebooksearch.firstrun",true);
 pref("ebooksearch.prompt",true);
 pref("extensions.{7585C31E-1E94-4498-ACEC-CB913A05FC52}.description", "chrome://ebooksearch/locale/ebooksearch.properties");
